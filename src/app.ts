569dc9ea6fc3396de94f173cea819d2e2218cc64
unknown <dsubhojit063@gmail.com>
2024-02-07 23:24:38 +0530

    setup done eslint husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: any, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
