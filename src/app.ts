37c81a73e8a71775ccd19fb38639e05ee6ea47f7
unknown <dsubhojit063@gmail.com>
2024-02-07 22:38:37 +0530

    install husky

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
