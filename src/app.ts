a7b93aefc8a7783d9f591c50a21d1737ca6a4b08
unknown <dsubhojit063@gmail.com>
2024-02-07 21:29:40 +0530

    [DONE setup mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, Application } from "express";
+import cors from "cors";
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
